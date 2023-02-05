e69b7b4d6652f3e74b77e17d2c613e8f99126992
KonstantinChirko <kostyachirko375@gmail.com>
2023-02-05 17:34:24 +0300

    initial commit

---
+++ src/main/java/com/chirko/onLine/OnLineApplication.java
@@ -0,0 +1,11 @@
+package com.chirko.onLine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnLineApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OnLineApplication.class, args);
+	}
+}

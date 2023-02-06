6d562f6d1cda7738d42173a11979ca198605152f
KonstantinChirko <kostyachirko375@gmail.com>
2023-02-06 14:04:25 +0300

    Setup flyway and data-jpa. Adding initial migration

---
+++ src/main/resources/db/migration/V1__init_DB.sql
@@ -0,0 +1,52 @@
+CREATE TYPE role AS ENUM ('ADMIN', 'CREATOR', 'USER');
+
+CREATE TABLE member
+(
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    alias         TEXT,
+    name          TEXT,
+    surname       TEXT,
+    login         TEXT NOT NULL UNIQUE,
+    password      TEXT NOT NULL,
+    birthday      DATE,
+    create_date   DATE NOT NULL    DEFAULT CURRENT_DATE,
+    modified_date DATE NOT NULL    DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE member_role
+(
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    member_id     UUID REFERENCES member,
+    role          ROLE NOT NULL    DEFAULT USER::role,
+    create_date   DATE             DEFAULT CURRENT_DATE,
+    modified_date DATE             DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE post
+(
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    member_id     UUID REFERENCES member,
+    text          TEXT,
+    create_date   DATE NOT NULL    DEFAULT CURRENT_DATE,
+    modified_date DATE NOT NULL    DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE img
+(
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    img           BYTEA NOT NULL,
+    post_id       UUID REFERENCES post,
+    create_date   DATE  NOT NULL   DEFAULT CURRENT_DATE,
+    modified_date DATE  NOT NULL   DEFAULT CURRENT_DATE
+);
+
+ALTER TABLE member ADD COLUMN avatar UUID REFERENCES img;
+
+CREATE TABLE comment
+(
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    member_id     UUID REFERENCES member,
+    text          TEXT NOT NULL,
+    create_date   DATE NOT NULL    DEFAULT CURRENT_DATE,
+    modified_date DATE NOT NULL    DEFAULT CURRENT_DATE
+);
